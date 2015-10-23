cd8c806951c74de2d6e45e222880862f9f37ee18
Anonim47 <yosef@yosefadi.tk>
2015-10-23 12:56:17 +0700

    Provider: Remove redefinition
    Change-Id: I16d56fab4fd8d12de1050be81d9a469a581d1f79

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3606,26 +3606,6 @@ public final class Settings {
         public static final String STATUSBAR_COMPONENT = "statusbar_component";
 
 	/**
-         * Custom System Animations
-         *
-         * @hide
-         */
-         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
-                  "activity_open",
-                  "activity_close",
-                  "task_open",
-                  "task_close",
-                  "task_to_front",
-                  "task_to_back",
-                  "wallpaper_open",
-                  "wallpaper_close",
-                  "wallpaper_intra_open",
-                  "wallpaper_intra_close",
-          };
-          public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-          public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
-
-	/**
          * Whether to show media art on lockscreen
          * @hide
          */
